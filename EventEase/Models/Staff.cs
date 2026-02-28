74228150180b737ed9dd1b37765a9fa6334a1592
Tokollo <ST10296818@rcconnect.edu.za>
2026-02-28 18:05:42 +0200

    More changes

---
+++ EventEase/Models/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Staff
+    {
+        [Key]
+        public int StaffId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = "Specialist"; // Admin or Specialist
+    }
+}
